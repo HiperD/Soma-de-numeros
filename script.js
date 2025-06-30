56424a44ca3a6ed9d6bcdeacdb7ddf29062ed667
Leonardo Silva e Cruz <101243664+HiperD@users.noreply.github.com>
2025-06-30 15:53:42 -0300

    teste 1

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('Seja bem-vindo a soma de numeros!');
+let numero = 0
+do{
+    numero = numero + parseInt(prompt("Digite o número que deseja somar"));
+    do{
+        numero = numero + parseInt(prompt("Digite outro numero que deseja somar"));
+    }while(confirm("Gostaria de somar mais numeros?"));
+alert(`Soma total resultou em ${numero}`)
+}while(confirm("Gostaria de somar mais um conjunto?"))
+alert("Até mais :)")
